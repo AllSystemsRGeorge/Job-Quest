5050f709dc2d36a8ac9933894e45233579f88613
liliankim <yejean831@gmail.com>
2022-08-04 19:53:06 -0700

    added API for quote

--- controllers/motivationalQ.js
+++ controllers/motivationalQ.js
@@ -1,20 +1,12 @@
 //api for motivational quote
 
-const axios = require("axios");
+const api_url ="https://zenquotes.io/api/today/";
 
-const options = {
-  method: 'POST',
-  url: 'https://motivational-quotes1.p.rapidapi.com/motivation',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'a3a4e80f16msh76859c9ae8fbee0p1baf97jsn009677e9d3e0',
-    'X-RapidAPI-Host': 'motivational-quotes1.p.rapidapi.com'
-  },
-  data: '{"key1":"value","key2":"value"}'
-};
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+getapi(api_url);
